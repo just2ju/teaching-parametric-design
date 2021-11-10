fd48e7017df976bc87be27b68174f686bf0abea2
Julia HvW <vonweyhernjulia@gmx.de>
2021-11-10 10:10:08 +0100

    corrected error

--- code/example copy/sketch.js
+++ code/example copy/sketch.js
@@ -1,41 +1,19 @@
-let xValue;
-let colorValue;
-
-let yValue = 5
-let textColor = ('red')
-
-
-function setup() {
-  createCanvas(400, 400);
-  frameRate(5)
+/*function setup() {
+  createCanvas(400, 400)
 }
 
 function draw() {
+  noLoop();
   background(255);
-  stroke('red');
-  fill('white');
-  rect(155, 155, 90, 90);
-  fill('black');
-  circle(
-    random(180, 220),
-    random(180, 220),
-    50
-  );
-}
+  noFill();
+  stroke(0);
+  strokeWeight(1);
 
-/*function draw() {
-  noLoop()
-  background(255);
-  fill('blue');
-  for (let y = 0; y < 10; y += 1) {
-    for (let x = 0; x < 10; x += 1) {
-    fill(random(0, 255), random(0, 255), random(0, 255), random(0, 255))
-    circle( 
-      100 + x * 20
-      100 + y * 20
-      x + y
-     );
-    }
-  }
+  beginShape();
+curveVertex(0, 0);
+curveVertex(40, 0);
+curveVertex(40, 20);
+curveVertex(0, 20);
+endShape(CLOSE);
+
 }*/
-
--- code/task-02.1/sketch.js
+++ code/task-02.1/sketch.js
@@ -3,5 +3,35 @@ function setup() {
 }
 
 function draw() {
-  background(100);
+  frameRate(10);
+  background(255);
+  noFill();
+  stroke('red');
+  strokeWeight(3);
+
+  beginShape();
+  for(let angle = 0; angle < 360; angle += 15) {
+    const radius = random(40, 80);
+    const x = radius * cos(Math.PI / 180 * angle);
+    const y = radius * sin(Math.PI / 180 * angle);
+    curveVertex(x + 200, y + 200);
+    }
+    endShape();
+
+    frameRate(5);
+  fill('lightblue');
+  stroke('blue');
+  strokeWeight(3);
+
+    beginShape();
+  for(let angle = 0; angle < 360; angle += 15) {
+    const radius = random(40, 80);
+    const x = radius * cos(Math.PI / 180 * angle);
+    const y = radius * sin(Math.PI / 180 * angle);
+    curveVertex(x + 200, y + 200);
+    }
+    endShape();
+
+    
 }
+
