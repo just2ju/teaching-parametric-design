b065c6ed63c75a39ac09491c6c7b4ae5c957f427
Julia HvW <vonweyhernjulia@gmx.de>
2021-11-17 10:04:06 +0100

    commit

---
+++ code/task-04/sketch.js
@@ -0,0 +1,13 @@
+const sketchWidth = 400;
+const sketchHeight = 400;
+
+function setup() {
+  createCanvas(sketchWidth, sketchHeight);
+}
+
+function draw() {
+  background(100);
+
+  translate(sketchWidth/2,  sketchHeight/2);
+  
+}
