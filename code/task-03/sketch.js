d9ceb32263ec270a700e0af6e10a07308de1c4bc
Julia HvW <vonweyhernjulia@gmx.de>
2021-11-17 09:53:37 +0100

    last commit

--- code/task-03/sketch.js
+++ code/task-03/sketch.js
@@ -1,31 +1,74 @@
-let a;
+const sketchWidth = 400;
+const sketchHeight = 400;
 
 function setup() {
-  createCanvas(400, 400);
-  background(255);
-  a = height / 2;
-  strokeWeight(0);
-
+  createCanvas(sketchWidth, sketchHeight);
 }
+
+let rotation = 0;
+
+
+
 function draw() {
-  stroke(0);
-  strokeWeight(0);
-  fill(0);
-  rect(80, 80, 320, 100);
-  rect(80, 80, 100, 200);
-  rect(180, 280, 100);
+  
+  noStroke();
+  fill('orange');
+  
+  translate(sketchWidth/2, sketchHeight/2);
+  rotate(Math.PI / 180 * rotation);
+  scale(0.6);
 
-  stroke(56, 151, 53);
-  strokeWeight(1);
-  fill(0, 255, 0);
-  circle(a - 19, 280, 200);
-  a = a + 0.9;
-  if (a < 0) {
-    a = height;
+  let segments = 5;
+  beginShape();
+  for (let c = 0; c < segments; c += 1) {
+    const rad = Math.PI / 180 * 360 / segments * c;
+    const x = 100 * Math.cos(rad);
+    const y = 100 * Math.sin(rad);
+    vertex(x, y);
   }
-  else if (a > 550) {
-    a = height / 2
-    a = a - 0.9
-  }
+  endShape();
 
+  beginShape();
+  fill('aquamarine')
+  for (let c = 0; c < segments; c += 1) {
+    const rad = Math.PI / 2 * 25 / segments * c;
+    const x = -33 * Math.cos(rad);
+    const y = -50 * Math.sin(rad);
+    vertex(x, y);
+  }
+  endShape();
+
+  beginShape();
+  fill('white')
+  for (let c = 0; c < segments; c += 1) {
+    const rad = Math.PI / 10 * 20 / segments * c;
+    const x = 40 * Math.cos(rad);
+    const y = 20 * Math.sin(rad);
+    vertex(x, y);
+  }
+  endShape();
+  
+  beginShape();
+  fill('blue');
+  scale(0.5);
+  for (let c = 0; c < segments; c += 1) {
+    const rad = Math.PI / 5 * 60 / segments * c;
+    const x = 20 * Math.cos(rad);
+    const y = 40 * Math.sin(rad);
+    vertex(x, y);
+  }
+  endShape();
+
+  beginShape();
+  fill('black');
+  scale(0.3);
+  for (let c = 0; c < segments; c += 1) {
+    const rad = Math.PI / 5 * 60 / segments * c;
+    const x = 20 * Math.cos(rad);
+    const y = 40 * Math.sin(rad);
+    vertex(x, y);
+  }
+  endShape();
+
+  rotation += 0.09;
 }
